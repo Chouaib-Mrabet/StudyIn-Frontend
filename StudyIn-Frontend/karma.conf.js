222477d45c911bf0cb1a2923cd3826caf2ffbb71
medazizammari <ammarimedaziz@gmail.com>
2022-01-22 10:02:05 +0000

    Project Initialization

--- StudyIn-Frontend/karma.conf.js
+++ StudyIn-Frontend/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/study-in'),
+      dir: require('path').join(__dirname, './coverage/study-in-frontend'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- StudyIn-Frontend/src/app/app.component.spec.ts
+++ StudyIn-Frontend/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'study-in'`, () => {
+  it(`should have as title 'StudyIn-Frontend'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('study-in');
+    expect(app.title).toEqual('StudyIn-Frontend');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('study-in app is running!');
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.content span')?.textContent).toContain('StudyIn-Frontend app is running!');
   });
 });
